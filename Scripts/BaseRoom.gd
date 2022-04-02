ef920c60663188ac76288c95f40d7d188a69829c
Alexandre Zenon <a.zenon@student.uclouvain.be>
2022-04-02 22:32:33 +0200

    Scene transition

--- Scripts/BaseRoom.gd
+++ Scripts/BaseRoom.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
+signal chose
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
+var timer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	timer = Timer.new()
+	timer.set_wait_time(2)
+	timer.connect("timeout", self, "on_timer_timeout")
+	add_child(timer)
+	timer.start()
+	print("TRUITE")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
-#	pass
+#	pa
+
+func on_timer_timeout():
+	print("TIMEOUT")
+	emit_signal("chose", "exit")
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -13,12 +13,36 @@ var water_items_count := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var instance = building_scene.instance()
-	add_child(instance)
+	create_level_instance()
 	
 
-
+func _on_chose(choice: String) -> void:
+	free_all_children()
+	match choice:
+		"door":
+			create_room_instance()
+			
+		"stairs":
+			create_level_instance()
+			
+		"exit":
+			create_level_instance()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+func create_level_instance() -> void:
+	var instance = building_scene.instance()
+	instance.get_node("RoomPlayer").connect("chose", self, "_on_chose")
+	add_child(instance)
+
+func create_room_instance() -> void:
+	var instance = room_scene.instance()
+	instance.connect("chose", self, "_on_chose")
+	add_child(instance)
+	
+func free_all_children() -> void:
+	for child in get_children():
+		child.queue_free()
+	
