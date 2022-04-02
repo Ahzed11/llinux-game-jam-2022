36851ed7bbf2a6fbb06fdd8a87a224ad5242325e
Alexandre Zenon <a.zenon@student.uclouvain.be>
2022-04-03 00:11:22 +0200

    Rooms

--- Scripts/BaseRoom.gd
+++ Scripts/BaseRoom.gd
@@ -2,22 +2,43 @@ extends Node2D
 
 signal chose
 
+onready var time_label := $CanvasLayer/Label as Label
 var timer: Timer
+const ROOM_FOLDER := "res://Scenes/Prefabs/Rooms/"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var room_path = get_random_room_path()
+	var room = load(ROOM_FOLDER + room_path)
+	var room_instance = room.instance()
+	add_child(room_instance)
+	create_timer()
+	
+func _process(delta: float) -> void:
+	time_label.set_text(str(round(timer.get_time_left())))
+
+func create_timer():
 	timer = Timer.new()
-	timer.set_wait_time(2)
+	timer.set_wait_time(rand_range(4, 5))
 	timer.connect("timeout", self, "on_timer_timeout")
 	add_child(timer)
 	timer.start()
-	print("TRUITE")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pa
+	
+func get_random_room_path() -> String:
+	var dir := Directory.new()
+	dir.open(ROOM_FOLDER)
+	dir.list_dir_begin()
+	var files = Array()
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif file.ends_with(".tscn"):
+			files.append(file)
+			
+	var rng := RandomNumberGenerator.new()
+	rng.randomize()
+	return files[rng.randi_range(0, len(files) - 1)]
 
 func on_timer_timeout():
-	print("TIMEOUT")
 	emit_signal("chose", "exit")
