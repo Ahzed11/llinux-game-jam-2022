b38473a594a53aa771e28b89c730dd1ca5e2ba48
richrad de j <rdejamblinne@student.uclouvain.be>
2022-04-02 23:36:26 +0200

    windows in levelscene

--- Scripts/SpawnManager.gd
+++ Scripts/SpawnManager.gd
@@ -1,28 +1,46 @@
 extends Node2D
 
-var children:Array
+var sol:Array
+var wall:Array
 
 func spawn_door(var rng):
 	var pose_number:int
 	var node = preload("res://Scenes/objet/door.tscn").instance()
 	pose_number = rng.randi_range(0,7)
-	while children[pose_number].get_child_count() != 0 :
+	while sol[pose_number].get_child_count() != 0 :
 		pose_number = rng.randi_range(0,7)
-	children[pose_number].add_child(node)
+	sol[pose_number].add_child(node)
 
 func spawn_stair(var rng):
 	var pose_number:int
 	var node = preload("res://Scenes/objet/stairs.tscn").instance()
 	pose_number = rng.randi_range(0,7)
-	while children[pose_number].get_child_count() != 0 :
+	while sol[pose_number].get_child_count() != 0 :
 		pose_number = rng.randi_range(0,7)
-	children[pose_number].add_child(node)
+	sol[pose_number].add_child(node)
+
+func spawn_windows(var rng):
+	var pose_number:int
+	var node
+	var number_windows = rng.randi_range(0,5)
+	
+	for x in range(number_windows):
+		if rng.randi_range(0,200) < 190 :
+			node = preload("res://Scenes/objet/window.tscn").instance()
+		else:
+			node = preload("res://Scenes/objet/window2.tscn").instance() 
+		pose_number = rng.randi_range(0,6)
+		while wall[pose_number].get_child_count() != 0 :
+			pose_number = rng.randi_range(0,6)
+		wall[pose_number].add_child(node)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	children = [$P2D1,$P2D2,$P2D3,$P2D4,$P2D5,$P2D6,$P2D7,$P2D8]
-	var rng = RandomNumberGenerator.new()
+	sol = [$sol1,$sol2,$sol3,$sol4,$sol5,$sol6,$sol7,$sol8]
+	wall = [$wall,$wall2,$wall3,$wall4,$wall5,$wall6,$wall7]
+	var rng := RandomNumberGenerator.new()
 	rng.randomize()
 	spawn_door(rng)
 	spawn_stair(rng)
+	spawn_windows(rng)
