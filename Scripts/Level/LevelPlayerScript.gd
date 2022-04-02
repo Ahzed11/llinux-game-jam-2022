eefcd514e73c7506148ce8454fc5da92807b1098
richrad de j <rdejamblinne@student.uclouvain.be>
2022-04-02 21:17:15 +0200

    staire + action

--- Scripts/Level/LevelPlayerScript.gd
+++ Scripts/Level/LevelPlayerScript.gd
@@ -2,12 +2,17 @@ extends KinematicBody2D
 
 export var move_speed := 400
 var player_input: Vector2
+var body_on
 
 func _ready() -> void:
 	player_input = Vector2(0, 0)
 
 func _process(delta: float) -> void:
 	player_input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	if Input.is_action_just_pressed("action1") :
+		body_on = $Area2D.get_overlapping_areas()
+		if len(body_on) > 0 :
+			print(str(body_on[0].name))
 
 func _physics_process(delta: float) -> void:
 	move_and_slide(player_input.normalized() * move_speed)
--- Scripts/SpawnManager.gd
+++ Scripts/SpawnManager.gd
@@ -10,6 +10,14 @@ func spawn_door(var rng):
 		pose_number = rng.randi_range(0,4)
 	children[pose_number].add_child(node)
 
+func spawn_stair(var rng):
+	var pose_number:int
+	var node = preload("res://Scenes/objet/stairs.tscn").instance()
+	pose_number = rng.randi_range(0,4)
+	while children[pose_number].get_child_count() != 0 :
+		pose_number = rng.randi_range(0,4)
+	children[pose_number].add_child(node)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,3 +25,4 @@ func _ready():
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	spawn_door(rng)
+	spawn_stair(rng)
