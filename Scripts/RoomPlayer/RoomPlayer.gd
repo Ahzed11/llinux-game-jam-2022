79b87445ec58ed661f3265b3c7a7ab66422ec904
Alexandre Zenon <a.zenon@student.uclouvain.be>
2022-04-02 15:54:57 +0200

    Player movement

---
+++ Scripts/RoomPlayer/RoomPlayer.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+export var move_speed := 400
+var player_input: Vector2
+
+func _ready() -> void:
+	player_input = Vector2(0, 0)
+
+func _process(delta: float) -> void:
+	player_input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	player_input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+
+func _physics_process(delta: float) -> void:
+	move_and_slide(player_input.normalized() * move_speed)
