920c49b882ce0986e03cf0d71eacd3ee1a81e3fa
Alexandre Zenon <a.zenon@student.uclouvain.be>
2022-04-02 16:17:05 +0200

    Player Weapon

---
+++ Scripts/RoomPlayer/RoomPlayerWeapon.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# if Input.is_action_just_pressed("action1"):
+		# for body in get_overlapping_bodies():
+	pass
