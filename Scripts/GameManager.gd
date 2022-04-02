7e68871d421238cfa66a84777e67c6e97dac5fdc
Alexandre Zenon <a.zenon@student.uclouvain.be>
2022-04-02 20:38:50 +0200

    Temp

---
+++ Scripts/GameManager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var thirst := 100
+var hungriness := 100
+
+var wood_items_count := 0
+var food_items_count := 0
+var water_items_count := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
