fe5c4254ea364d419314350a4e6fd7a9f50e3881
richrad de j <rdejamblinne@student.uclouvain.be>
2022-04-03 04:01:05 +0200

    better pleyer mouvement

--- Scripts/Level/LevelPlayerScript.gd
+++ Scripts/Level/LevelPlayerScript.gd
@@ -2,14 +2,29 @@ extends KinematicBody2D
 
 signal chose
 
-export var move_speed := 400
+export var move_speed := 100
 var player_input: Vector2
 var body_on
+var to_right := true
 
 func _ready() -> void:
 	player_input = Vector2(0, 0)
 
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("right") and not to_right:
+		to_right = true
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("left") and to_right:
+		to_right = false
+		$Sprite.flip_h = true
+
+		
+	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
+		$Sprite.play("default")
+	else:
+		$Sprite.stop()
+
+	
 	player_input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	if Input.is_action_just_pressed("action1") :
 		body_on = $Area2D.get_overlapping_areas()
