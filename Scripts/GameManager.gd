f8466275120da6531a46f32b0c230730ddebc342
Alexandre Zenon <a.zenon@student.uclouvain.be>
2022-04-03 06:06:27 +0200

    UI

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -5,6 +5,13 @@ class_name GameManager
 var building_scene = preload("res://Scenes/LevelScene.tscn")
 var room_scene = preload("res://Scenes/Prefabs/BaseRoom.tscn")
 
+onready var canvas_layer = $CanvasLayer as CanvasLayer
+onready var food_label = $CanvasLayer/VBoxContainer/HBoxContainer/FoodLabel as Label
+onready var water_label = $CanvasLayer/VBoxContainer/HBoxContainer2/WaterLabel as Label
+onready var wood_label = $CanvasLayer/VBoxContainer/HBoxContainer3/WoodLabel as Label
+onready var water_bar = $CanvasLayer/HBoxContainer2/WaterBar as ProgressBar
+onready var food_bar = $CanvasLayer/HBoxContainer2/FoodBar as ProgressBar
+
 var thirst := 100
 var hungriness := 100
 var wood_items_count := 1
@@ -20,7 +27,11 @@ const ORIGIN = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	create_level_instance()
-	
+	update_water_label()
+	update_food_label()
+	update_wood_label()
+	update_water_bar()
+	update_food_bar()
 
 func _on_chose(choice: String) -> void:
 	match choice:
@@ -33,6 +44,9 @@ func _on_chose(choice: String) -> void:
 			room.reset_player_pos()
 			room.start_timer()
 			
+			for child in canvas_layer.get_children():
+				child.hide()
+			
 		"stairs":
 			level = null
 			room = null
@@ -40,25 +54,27 @@ func _on_chose(choice: String) -> void:
 			create_level_instance()
 			
 		"exit":
+			for child in canvas_layer.get_children():
+				child.show()
+			
 			remove_child(room)
 			add_child(level)
 	
 		"water":
-			water_items_count -= 1
+			dec_water()
 			if water_items_count < 1 :
-				print("send")
 				emit_signal("no_more","water")
 		"food": 
-			food_items_count -= 1
+			dec_food()
 			
 func _on_loot(loot: String) -> void:
 	match loot:
 		"food":
-			food_items_count += 1
+			inc_food()
 		"water":
-			water_items_count += 1
+			inc_water()
 		"wood":
-			wood_items_count += 1
+			inc_wood()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
@@ -79,3 +95,59 @@ func free_all_children() -> void:
 	for child in get_children():
 		child.queue_free()
 	
+func update_food_label() -> void:
+	food_label.text = str(food_items_count)
+	
+func inc_food() -> void:
+	food_items_count += 1
+	update_food_label()
+	
+func dec_food() -> void:
+	food_items_count -= 1
+	update_food_label()
+	
+func update_water_label() -> void:
+	water_label.text = str(water_items_count)
+	
+func inc_water() -> void:
+	water_items_count += 1
+	update_water_label()
+	
+func dec_water() -> void:
+	water_items_count -= 1
+	update_water_label()
+	
+func update_wood_label() -> void:
+	wood_label.text = str(wood_items_count)
+	
+func inc_wood() -> void:
+	wood_items_count += 1
+	update_wood_label()
+	
+func dec_wood() -> void:
+	wood_items_count -= 1
+	update_wood_label()
+	
+func update_water_bar() -> void:
+	water_bar.value = thirst
+	
+func decrease_thirst_by(amount: int) -> void:
+	thirst -= amount
+	update_water_bar()
+	
+func increase_thirst_by(amount: int) -> void:
+	thirst += amount
+	thirst = min(100, thirst)
+	update_water_bar()
+	
+func update_food_bar() -> void:
+	food_bar.value = hungriness
+	
+func decrease_hungriness_by(amount: int) -> void:
+	hungriness -= amount
+	update_food_bar()
+	
+func increase_hungriness_by(amount: int) -> void:
+	hungriness += amount
+	hungriness = min(100, hungriness)
+	update_food_bar()
