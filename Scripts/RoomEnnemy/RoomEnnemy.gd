f65fdfda55bc115a6e76fa3f5d29173221ded069
richrad de j <rdejamblinne@student.uclouvain.be>
2022-04-03 08:18:19 +0200

    loot on dead man

--- Scripts/RoomEnnemy/RoomEnnemy.gd
+++ Scripts/RoomEnnemy/RoomEnnemy.gd
@@ -1,11 +1,18 @@
 extends Human
 
+signal loot
+
 class_name Ennemy
+onready var game_manager = get_tree().get_root().get_node("GameManager")
 
 func _ready() -> void:
-	pass # Replace with function body.
+	connect("loot", game_manager, "_on_loot")
+	print(game_manager)
 
 
 
 func _on_Ennemy_is_dead() -> void:
+	emit_signal("loot", "wood")
+	emit_signal("loot", "food")
+	
 	queue_free()
